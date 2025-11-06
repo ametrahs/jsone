c585f0242b18b68d8a32be3b626b02306596d71f
Eneb <135270219+ametrahs@users.noreply.github.com>
2025-11-06 11:42:36 -0800

    basicjs

---
+++ app.js
@@ -0,0 +1,23 @@
+// **** learning js basics by super simple dev ****
+
+// alert('hi eneb')
+
+// document.body.innerHTML = "<h1>Hello from JS</h1>"
+
+// document.body.style.backgroundColor = "lightblue"
+
+// document.body.style.textAlign = "center"
+
+// exersises
+// alert('good morning')
+
+// alert('i am eneb,z future billionaire inshaallah')
+
+// console.log(10+5)
+// console.log(20-5)
+// console.log(2+2-5)
+
+// document.body.innerHTML ="<h1>good mornimg</h1>"
+// document.body.innerHTML = "<h1>i am eneb,z future billionaire inshaallah</h1>";
+// document.body.innerHTML = "<h1>...</h1>";
+
